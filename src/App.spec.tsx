bf772e7982d9946ac492b7a0314ebc6727ce4ce2
rafaelvm <vieiramrafael@gmail.com>
2023-01-25 21:37:28 -0300

    feat: added jest config, babel and testing library

---
+++ src/App.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("render App", () => {
+  it("should render", () => {
+    expect(1).toBe(1);
+  });
+
+  it("should display elements", () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole("heading", { name: /vite \+ react/i })
+    ).toBeInTheDocument();
+  });
+});
