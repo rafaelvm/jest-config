b5b295d64efbdb00210c23ba98666c6b8a2acfe2
rafaelvm <vieiramrafael@gmail.com>
2023-02-06 17:21:20 -0300

    fix: adjusted plop generator

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -11,22 +11,22 @@ module.exports = plop => {
     actions: [
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}.component.tsx',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.component.tsx',
         templateFile: './templates/components/components.tsx.hbs',
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}.styles.ts',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.styles.ts',
         templateFile: './templates/components/styles.ts.hbs',
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}.types.ts',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.types.ts',
         templateFile: './templates/components/types.ts.hbs',
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}.spec.tsx',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
         templateFile: './templates/components/spec.tsx.hbs',
       },
     ],
