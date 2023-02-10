461844f28409570ac59aa864c7acffad7e5f7499
rafaelvm <vieiramrafael@gmail.com>
2023-02-09 22:32:35 -0300

    chore: added theme provider and global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  height: auto;
+}
+
+html {
+  @media (max-width: 1080px) {
+    font-size: 93.75%;
+  }
+  @media (max-width: 720px) {
+    font-size: 87.5%;
+  }
+}
+
+button {
+  cursor: pointer;
+}
+
+[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+  &:hover {
+    opacity: 0.2;
+  }
+}
+
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    background: '#F0F2F5',
+    black: '#444444',
+    darkBlue: '#005f6b',
+    mediumBlue: '#008c9e',
+    ciano: '#00b4cc',
+    lightBlue: '#00dffc',
+  },
+}
